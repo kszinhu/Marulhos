e1d92fd6e99f3897ec1a43033290e8b675e98f3a
kszinhu - windows <rodrigues.cassiano2007@gmail.com>
2022-12-29 00:02:53 -0300

    build: add CI/CD actions and update function generateKeys

--- api/bin/generateKeys.js
+++ api/bin/generateKeys.js
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 import { writeFileSync } from "fs";
 
-function generateKeypair() {
+function generateKeyPair() {
   // @ts-ignore
   return crypto.generateKeyPairSync("rsa", {
     modulusLength: 2048,
@@ -17,10 +17,43 @@ function generateKeypair() {
   });
 }
 
-const { publicKey, privateKey } = generateKeypair();
+function writeFileSyncRecursive(filename, content, charset) {
+  // -- normalize path separator to '/' instead of path.sep, 
+  // -- as / works in node for Windows as well, and mixed \\ and / can appear in the path
+  let filepath = filename.replace(/\\/g,'/');  
+
+  // -- preparation to allow absolute paths as well
+  let root = '';
+  if (filepath[0] === '/') { 
+    root = '/'; 
+    filepath = filepath.slice(1);
+  } 
+  else if (filepath[1] === ':') { 
+    root = filepath.slice(0,3);   // c:\
+    filepath = filepath.slice(3); 
+  }
+
+  // -- create folders all the way down
+  const folders = filepath.split('/').slice(0, -1);  // remove last item, file
+  folders.reduce(
+    (acc, folder) => {
+      const folderPath = acc + folder + '/';
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+      }
+      return folderPath
+    },
+    root // first 'acc', important
+  ); 
+  
+  // -- write file
+  fs.writeFileSync(root + filepath, content, charset);
+}
+
+const { publicKey, privateKey } = generateKeyPair();
 
 console.log(publicKey);
 console.log(privateKey);
 
-writeFileSync("./keys/public.pem", publicKey, { encoding: "utf8" });
-writeFileSync("./keys/private.pem", privateKey, { encoding: "utf8" });
+writeFileSyncRecursive("./keys/public.pem", publicKey, { encoding: "utf8" });
+writeFileSyncRecursive("./keys/private.pem", privateKey, { encoding: "utf8" });
